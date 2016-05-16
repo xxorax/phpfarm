15dfaffb1340ab6b1ead2f5a8750ff5a667a3b25
Martin Panel <martin@xorax.info>
2016-05-16 16:31:35 +0700

    Add many extensions

--- src/options.sh
+++ src/options.sh
@@ -14,21 +14,68 @@ vmajor=$2
 vminor=$3
 vpatch=$4
 
+
+#--with-gmp=/usr/include/x86_64-linux-gnu \
+#configure: error: Unable to locate gmp.h
+#
+
 #gcov='--enable-gcov'
 configoptions="\
 --enable-bcmath \
+--enable-debug
 --enable-calendar \
+--enable-cli \
+--enable-gd-native-ttf \
 --enable-exif \
+--enable-fastcgi \
+--enable-fpm \
 --enable-ftp \
+--enable-gd-native-ttf \
+--enable-inline-optimization \
+--enable-intl \
+--enable-json \
+--enable-libxml \
+--enable-mbregex \
 --enable-mbstring \
+--enable-mysql \
+--enable-mysqli \
+--enable-mysqlnd \
 --enable-pcntl \
+--enable-pdo \
+--enable-posix \
 --enable-soap \
 --enable-sockets \
 --enable-sqlite-utf8 \
+--enable-sysvsem \
+--enable-sysvshm \
 --enable-wddx \
 --enable-zip \
+--with-bz2 \
+--with-curl \
+--with-fpm-group=www-data \
+--with-fpm-user=www-data \
+--with-freetype-dir \
 --with-zlib \
+--with-gd \
 --with-gettext \
+--with-jpeg-dir \
+--with-mcrypt \
+--with-mhash \
+--with-mysql \
+--with-mysqli \
+--with-openssl \
+--with-pcre-regex \
+--with-pdo-mysql \
+--with-pdo-pgsql \
+--with-pear \
+--with-pgsql \
+--with-png-dir \
+--with-posix \
+--with-vpx-dir \
+--with-xmlrpc \
+--with-xsl \
+--with-zlib \
+--with-zlib-dir \
 $gcov"
 
 echo $version $vmajor $vminor $vpatch
